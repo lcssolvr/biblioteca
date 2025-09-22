be098bae7c174ae58859dc8fa8508379212f7695
Lucas Oliveira <lucas.santos661@hotmail.com>
2025-09-22 12:47:47 -0300

    Configurações do projeto

---
+++ src/main/kotlin/lcssolvr/com/biblioteca/config/WebConfig.kt
@@ -0,0 +1,20 @@
+package ubsocial.com.biblioteca.config
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig {
+    
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(regisrty: CorsRegistry) {
+                regisrty.addMapping("/**")
+                    .allowedOrigins("http://localhost:8081")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/lcssolvr/com/biblioteca/BibliotecaApplicationTests.kt
@@ -0,0 +1,13 @@
+package lcssolvr.com.biblioteca
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
